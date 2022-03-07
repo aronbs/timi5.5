e9cd35b5cc751403b586a086cccc5184c1cb0aab
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-07 20:40:49 +0000

    Initial commit

---
+++ bin/timi5.dart
@@ -0,0 +1,5 @@
+import 'package:timi5/timi5.dart' as timi5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${timi5.calculate()}!');
+}
---
+++ test/timi5_test.dart
@@ -0,0 +1,8 @@
+import 'package:timi5/timi5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
